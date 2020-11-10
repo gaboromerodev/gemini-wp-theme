b9c20849607b0d7a1121f2ace20b4ada2bd58db1
Gabriel Adrian Romero <gabrieladrianromero28@gmail.com>
2020-11-10 18:13:45 -0300

    Added style functions. Added filemtime to our styles function to avoid browser caching issues

--- app/public/wp-content/themes/gemini/functions.php
+++ app/public/wp-content/themes/gemini/functions.php
@@ -4,3 +4,17 @@
  * 
  * @package Gemini
  */
+
+//  echo '<pre>';
+//  print_r( filemtime( get_template_directory() . '/style.css' ));
+//  wp_die();
+// The above is for test our functions
+
+function gemini_enqueue_scripts() {
+    // wp_enqueue_style( 'stylesheet', get_template_directory_uri() . '/main.css', ['stylesheet'] ); //This declare a dependency meaning it will load after style.css
+    wp_enqueue_style( 'stylesheet', get_stylesheet_uri(), [], filemtime( get_template_directory() . '/style.css' ), 'all');
+}
+
+add_action( 'wp_enqueue_scripts', 'gemini_enqueue_scripts' );
+
+?>
