53b37bea50f73e7a0eed70ca4331ca56a18f21fe
Gabriel Adrian Romero <gabrieladrianromero28@gmail.com>
2020-11-14 21:23:30 -0300

    Add theme support for background, thumbnails, html5, bloc styles, wide images and set global width for content

--- app/public/wp-content/themes/gemini/inc/classes/class-gemini-theme.php
+++ app/public/wp-content/themes/gemini/inc/classes/class-gemini-theme.php
@@ -31,7 +31,51 @@
      }
 
      public function setup_theme() {
-         add_theme_support( 'title-tag' );
+        add_theme_support( 'title-tag' );
+
+        add_theme_support( 'custom-logo', [
+            'header-text' => [ 'site-title', 'site-description' ],
+            'height'      => 100,
+            'width'       => 400,
+            'flex-height' => true,
+            'flex-width'  => true,
+        ]);
+
+        add_theme_support( 'custom-background', [
+            'default-color' => '000000',
+            'default-image' => '',
+            'default-repeat' => 'no-repeat',
+        ]);
+
+        add_theme_support( 'post-thumbnails' );
+
+        add_theme_support( 'customize-selective-refresh-widgets' );
+
+        add_theme_support( 'automatic-feed-links' );
+
+        add_theme_support( 
+            'html5', 
+             [
+                'comment-list', 
+                'comment-form', 
+                'search-form', 
+                'gallery', 
+                'caption', 
+                'style', 
+                'script',
+            ]
+        );
+
+        add_editor_style();
+        add_theme_support( 'wp-block-styles' );
+
+        add_theme_support( 'align-wide' );
+
+        global $content_width;
+        if (! isset($content_width)) {
+            $content_width = 1024;
+        }
+
      }
 
  }
