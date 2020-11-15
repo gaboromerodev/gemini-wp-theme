b76ddc8797724afdf94ba0160d27ee95183f7f02
Gabriel Adrian Romero <gabrieladrianromero28@gmail.com>
2020-11-15 13:33:34 -0300

    Add dinamic navbar Menus and submenus

--- app/public/wp-content/themes/gemini/inc/classes/class-menus.php
+++ app/public/wp-content/themes/gemini/inc/classes/class-menus.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Register menus
+ * Register Menus
  * 
  * @package Gemini
  */
@@ -37,18 +37,26 @@
      public function get_menu_id( $location ) {
          // Get all location
          $locations = get_nav_menu_locations();
-        //  echo '<pre>';
-        //  print_r( $locations );
-        //  wp_die();
-
+      
         // Get object id by location
         $menu_id = $locations[$location];
 
-        // echo '<pre>';
-        // print_r( $menu_id );
-        // wp_die();
-
         return ! empty( $menu_id ) ? $menu_id : '';
+     }
+
+     public function get_child_menu_items( $menu_array, $parent_id ) {
+        
+        $child_menus = [];
+
+         if ( ! empty( $menu_array ) && is_array( $menu_array )) {
+             foreach ( $menu_array as $menu ) {
+                 if ( intval( $menu->menu_item_parent) === $parent_id ) {
+                     array_push( $child_menus, $menu );
+                 }
+             }
+         }
+
+         return $child_menus;
      }
 
  }
