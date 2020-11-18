ab71d40bf0053ca9dfad376ee41dc120911918ea
Gabriel Adrian Romero <gabrieladrianromero28@gmail.com>
2020-11-18 00:39:15 -0300

    Add blog page title

--- app/public/wp-content/themes/gemini/index.php
+++ app/public/wp-content/themes/gemini/index.php
@@ -15,6 +15,21 @@ get_header();
                     ?>
                     <div class="container">
                         <?php
+                            if (is_home() && ! is_front_page()) {
+                                ?>
+                                    <header class="mb-5">
+                                        <h1 class="page-title screen-reader-text">
+                                            <?php single_post_title();?>
+                                        </h1>
+
+                                    </header>
+                                <?php
+                            }
+                        ?>
+
+
+
+                        <?php
                             while (have_posts() ) : the_post();
                             the_title();
                             the_content();
