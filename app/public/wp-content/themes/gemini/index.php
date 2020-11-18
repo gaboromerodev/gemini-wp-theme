84a0c856e1ca37c7ef5b8012adc496aa5681ed87
Gabriel Adrian Romero <gabrieladrianromero28@gmail.com>
2020-11-18 02:16:12 -0300

    Set up bootstrap grid layout

--- app/public/wp-content/themes/gemini/index.php
+++ app/public/wp-content/themes/gemini/index.php
@@ -1,46 +1,76 @@
 <?php
 /**
  * Main template file.
- * 
- * @package Gemini
+ *
+ * @package gemini
  */
+
 get_header();
 
- ?>
 
-    <div class="primary">
-        <main id="main" class="site-main mt-5" role="main">
-            <?php 
-                if( have_posts() ) {
-                    ?>
-                    <div class="container">
-                        <?php
-                            if (is_home() && ! is_front_page()) {
-                                ?>
-                                    <header class="mb-5">
-                                        <h1 class="page-title screen-reader-text">
-                                            <?php single_post_title();?>
-                                        </h1>
+?>
 
-                                    </header>
-                                <?php
+	<div id="primary">
+		<main id="main" class="site-main mt-5" role="main">
+			<?php
+			if ( have_posts() ) :
+				?>
+				<div class="container">
+					<?php
+					if ( is_home() && ! is_front_page() ) {
+						?>
+						<header class="mb-5">
+							<h1 class="page-title screen-reader-text">
+								<?php single_post_title(); ?>
+							</h1>
+						</header>
+						<?php
+					}
+					?>
+
+					<div class="row">
+						<?php
+						$index = 0;
+						$no_of_columns = 3;
+
+						while ( have_posts() ) : the_post();
+
+							if ( 0 === $index % $no_of_columns ) {
+								?>
+								<div class="col-4">
+								<?php
                             }
-                        ?>
+                            
+                            ?>
+                            <h3><?php the_title();?></h3>
+                            <div><?php the_content();?></div>
+                            <?php
 
+							
+							$index ++;
 
+							if ( 0 !== $index && 0 === $index % $no_of_columns ) {
+								?>
+								</div>
+								<?php
+							}
 
-                        <?php
-                            while (have_posts() ) : the_post();
-                            the_title();
-                            the_content();
-                            endwhile;
-                        ?>
-                    </div>
-                    <?php 
-                }
-            
-            ?>
-        </main>
-    </div>
-<?php 
+						endwhile;
+						?>
+					</div>
+				</div>
+			<?php
+
+			else :
+
+				get_template_part( 'template-parts/content-none' );
+
+			endif;
+
+		
+			?>
+		</main>
+	</div>
+
+<?php
 get_footer();
