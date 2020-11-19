1f3d5f164da5b27ea2bc91d031aeb8071a79c220
Gabriel Adrian Romero <gabrieladrianromero28@gmail.com>
2020-11-18 22:59:21 -0300

    Add template parts as components

--- app/public/wp-content/themes/gemini/template-parts/content.php
+++ app/public/wp-content/themes/gemini/template-parts/content.php
@@ -4,9 +4,14 @@
  * 
  * @package gemini
  */
-
-
  ?>
 
-<h3><?php the_title(); ?></h3>
-<div><?php the_content(); ?></div>
+<article id="post-<?php the_ID(); ?>" <?php post_class( 'mb-5' ); ?>>
+    <?php 
+        get_template_part( 'template-parts/components/blog/entry-header' );
+        get_template_part( 'template-parts/components/blog/entry-meta' );
+        get_template_part( 'template-parts/components/blog/entry-content' );
+        get_template_part( 'template-parts/components/blog/entry-footer' );
+    ?>
+
+</article>
